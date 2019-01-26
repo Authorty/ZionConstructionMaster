e2067539f8e9cc5f749059f3bdcff38ee1986ad1
Jody Bartelt <jdbartelt@hotmail.com>
2019-01-26 06:44:44 -0600

    Add project files.

---
+++ ZionConstructionMaster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZionConstructionMaster.Startup))]
+namespace ZionConstructionMaster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
